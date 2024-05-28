4f15fbe9c625cb4aa9b9b98b20e694a799163ace
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-05-28 17:06:36 -0500

    minor bugfix

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 import GUI from "lil-gui";
-import { debug } from "openai/core.mjs";
 import gsap from "gsap";
 
 const debugObject = {};
